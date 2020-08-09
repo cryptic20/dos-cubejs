b01d17d250b5620614b239a9c17ded05d58f4135
sherwin wyco <sherwin.wyco20@gmail.com>
2020-08-09 04:41:00 -0400

    added web generator

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const CubejsServer = require('@cubejs-backend/server');
 
 const server = new CubejsServer();
+const jwt = require('jsonwebtoken');
+const CUBE_API_SECRET= `${process.env.CUBEJS_API_SECRET}`;
+const cubejsToken = jwt.sign({}, CUBE_API_SECRET, { expiresIn: '30d' });
+console.log(cubejsToken)
+
 
 server.listen().then(({ version, port }) => {
   console.log(`🚀 Cube.js server (${version}) is listening on ${port}`);
