b2112e421b2fc640304577f63300c3664edcefa1
sherwin wyco <sherwin.wyco20@gmail.com>
2020-08-09 04:44:00 -0400

    added port listener

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const CubejsServer = require('@cubejs-backend/server');
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const CubejsServerCore = require('@cubejs-backend/server-core');
 
-const server = new CubejsServer();
+const app = express();
+app.use(require('cors')());
+app.use(bodyParser.json({ limit: '50mb' }));
+
 const jwt = require('jsonwebtoken');
 const CUBE_API_SECRET= `${process.env.CUBEJS_API_SECRET}`;
 const cubejsToken = jwt.sign({}, CUBE_API_SECRET, { expiresIn: '30d' });
 console.log(cubejsToken)
 
+const serverCore = CubejsServerCore.create();
+serverCore.initApp(app);
 
-server.listen().then(({ version, port }) => {
-  console.log(`🚀 Cube.js server (${version}) is listening on ${port}`);
-}).catch(e => {
-  console.error('Fatal error during server start: ');
-  console.error(e.stack || e);
+const port = process.env.PORT || 4000;
+app.listen(port, (err) => {
+  if (err) {
+    console.error('Fatal error during server start: ');
+    console.error(e.stack || e);
+  }
+  console.log(`🚀 Cube.js server (${CubejsServerCore.version()}) is listening on ${port}`);
 });
